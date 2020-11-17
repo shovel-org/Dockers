f77295de3207ba3c43385f8d1e0570a4562ba56b
Jakub Čábera <cabera.jakub@gmail.com>
2020-11-17 15:29:31 +0100

    rebuild 2020.11.17

--- build.ps1
+++ build.ps1
@@ -3,12 +3,12 @@
 # latest, windows-latest, 2009, windows-2009
 docker build --tag 'shovelinstaller/shovel:latest' --tag 'shovelinstaller/shovel:windows-latest' --tag 'shovelinstaller/shovel:2009' --tag 'shovelinstaller/shovel:windows-2009' --no-cache -f .\windows\Dockerfile .
 # 2004, windows-2004
-docker build --build-arg VERSION=2004 --tag 'shovelinstaller/shovel:2004' --tag 'shovelinstaller/shovel:windows-2004' --no-cache -f .\windows\Dockerfile .
+docker build --build-arg 'VERSION=2004' --tag 'shovelinstaller/shovel:2004' --tag 'shovelinstaller/shovel:windows-2004' --no-cache -f .\windows\Dockerfile .
 
 ##servercore
-docker build --build-arg POWERSHELL='pwsh.exe' --build-arg VERSION='windowsservercore-2004' --build-arg IMAGE='powershell' --tag 'shovelinstaller/shovel:windowsservercore' --tag 'shovelinstaller/shovel:windowsservercore-2004' --no-cache -f .\windows\Dockerfile .
+docker build --build-arg 'IMAGE=powershell' --build-arg 'VERSION=windowsservercore-2004' --build-arg 'POWERSHELL=pwsh.exe' --tag 'shovelinstaller/shovel:windowsservercore' --tag 'shovelinstaller/shovel:windowsservercore-2004' --no-cache -f .\windows\Dockerfile .
 
-docker push shovelinstaller/shovel
+docker push 'shovelinstaller/shovel'
 #endregion Windows
 
 #region Linux
---
+++ windows/install.ps1
@@ -0,0 +1,21 @@
+$ErrorActionPreference = 'Stop'
+
+# Shovel installation
+Invoke-WebRequest 'https://raw.githubusercontent.com/ScoopInstaller/Install/master/install.ps1' -OutFile "$env:TMP\install.ps1" -UseBasicParsing
+& "$env:TMP\install.ps1" -RunAsAdmin
+Get-ChildItem "${env:SCOOP}\shims" -Filter 'scoop.*' | Copy-Item -Destination { Join-Path $_.Directory.FullName (($_.BaseName -replace 'scoop', 'shovel') + $_.Extension) }
+
+# Remove trash config options
+shovel config rm 'rootPath'
+# Install dependencies and new powershell
+shovel install 'lessmsi' --global
+shovel config 'MSIEXTRACT_USE_LESSMSI' $true
+shovel install '7zip' 'mingit' 'gsudo' 'innounp' 'dark' 'pwsh' --global
+shovel config 'SCOOP_REPO' ${env:SCOOP_REPO}
+shovel update
+shovel config 'SCOOP_BRANCH' ${env:SCOOP_BRANCH}
+shovel update
+shovel status
+shovel cache rm '*'
+
+exit 0
