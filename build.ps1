a1e7dec62c4cd3938d370731fa5161c59a93d32d
Jakub Čábera <cabera.jakub@gmail.com>
2020-12-16 20:58:53 +0100

    build 2020-12-16
    685 builds
    shovel 0.55

--- build.ps1
+++ build.ps1
@@ -1,8 +1,9 @@
 #region Windows
+# shovel cache rm *; shovel download 7zip cwRsync dark EXEExplorer fiddler mingit git gsudo innounp lessmsi pwsh pwsh-preview RDPWrapper vim win32-openssh winget zstd; Copy-Item "$env:SCOOP\cache\*" '.\.cache' -Force
 Push-Location $PWD
 
 ## windows
-### latest, windows-latest, 20H2, windows-20H2
+### latest, windows, windows-latest, 20H2, windows-20H2
 docker build --tag 'shovelinstaller/shovel:latest' --tag 'shovelinstaller/shovel:windows' --tag 'shovelinstaller/shovel:windows-latest' --tag 'shovelinstaller/shovel:20H2' --tag 'shovelinstaller/shovel:windows-20H2' --no-cache -f .\windows\Dockerfile .
 ### 2009, windows-2009
 docker build --build-arg 'VERSION=2009' --tag 'shovelinstaller/shovel:2009' --tag 'shovelinstaller/shovel:windows-2009' --no-cache -f .\windows\Dockerfile .
@@ -12,7 +13,7 @@ docker build --build-arg 'VERSION=2004' --tag 'shovelinstaller/shovel:2004' --ta
 ##servercore
 docker build --build-arg 'IMAGE=powershell' --build-arg 'VERSION=windowsservercore-2004' --build-arg 'POWERSHELL=pwsh.exe' --tag 'shovelinstaller/shovel:windowsservercore' --tag 'shovelinstaller/shovel:windowsservercore-latest' --tag 'shovelinstaller/shovel:windowsservercore-2004' --no-cache -f .\windows\Dockerfile .
 
-docker push 'shovelinstaller/shovel'
+# docker push 'shovelinstaller/shovel'
 #endregion Windows
 
 #region Linux
